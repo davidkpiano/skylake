c31ff1e0b437e1a1a2978ad79197cabe3fd3ed88
ariiiman <contact@aristidebenoist.com>
2016-07-01 13:27:19 +0200

    1.0.10

--- src/core/animate.js
+++ src/core/animate.js
@@ -3,7 +3,8 @@
 RULES
 ─────
 
-Animate elements with class :
+BUG WITH ROLLUP AND DETECT NODELIST :
+for animate elements with class :
 ►►►  for one   →   passing dom element
 ►►►  for all   →   '.myClass'
 
@@ -26,6 +27,9 @@ a2.init()
 const a3 = new S.Animate('#id', '3dx', '0', '100', 'linear', 1000, {delay: {before: 500, after: 500}, callback: callback, during: duringCallback})
 a3.init()
 
+const a4 = new S.Animate('#id', ['rotate', '3dy'], [0, '0'], [-45, '-6'], 'Power4Out', 450)
+a4.init()
+
 */
 
 S.Animate = class {
@@ -38,7 +42,6 @@ S.Animate = class {
         this.duration = duration
         this._opts = options || false
 
-        this.distance = +this.end - +this.start
         this.delayBefore = this._opts.delay ? this._opts.delay.before : false
         this.delayAfter = this._opts.delay ? this._opts.delay.after : false
 
@@ -47,21 +50,7 @@ S.Animate = class {
         this.EasingLibrary = S.Easing
         this.raf = new S.RafIndex()
 
-        switch (this.param) {
-            case 'x':
-            case 'y':
-                this.getUpdate = this.attributUpdate
-                break
-            case '3dx':
-            case '3dy':
-                this.getUpdate = this.t3dUpdate
-                break
-            case 'scrollTop':
-                this.getUpdate = this.scrollTopUpdate
-                break
-            default:
-                this.getUpdate = this.styleUpdate
-        }
+        this.prepare()
 
         S.BindMaker(this, ['run', 'loop'])
     }
@@ -77,24 +66,85 @@ S.Animate = class {
     }
 
     loop () {
-        const currentTime      = S.Win.perfNow()
-        const multiplier       = (currentTime - this.startTime) / this.duration
-        const multiplierT      = multiplier > 1 ? 1 : multiplier // T → ternary
+        const currentTime = S.Win.perfNow()
+        const multiplier = (currentTime - this.startTime) / this.duration
+        const multiplierT = multiplier > 1 ? 1 : multiplier // T → ternary
         const easingMultiplier = this.EasingLibrary[this.easing](multiplierT)
-        const update           = +this.start + this.distance * easingMultiplier // Lerp → linear interpolation
+        let update
 
-        this.getUpdate(update)
+        // The linear interpolation → Lerp
+        if (this.isNotMultipleT) {
+            update = +this.start + this.distance * easingMultiplier
+        } else {
+            update = []
+            for (let i = 0; i < this.updateQty; i++) {
+                update[i] = +this.start[i] + this.distance[i] * easingMultiplier
+            }
+        }
+
+        this.render(update)
 
         if (multiplierT < 1) {
             this.raf.start(this.loop)
         } else {
             this.raf.cancel()
-            this.getUpdate(this.end)
+            this.render(this.end)
             if (this._opts.callback) {
                 const delay = this.delayAfter ? this.delayAfter : 0
                 S.Delay(this._opts.callback, delay)
             }
         }
+    }
+
+    prepare () {
+        if (this.param.constructor === Array) {
+            this.isNotMultipleT = false
+            this.updateQty = this.param.length
+            this.render = this.multipleT
+            this.distance = []
+            for (let i = 0; i < this.updateQty; i++) {
+                this.distance[i] = +this.end[i] - +this.start[i]
+            }
+        } else {
+            switch (this.param) {
+                case 'x':
+                case 'y':
+                    this.render = this.attributUpdate
+                    break
+                case '3dx':
+                case '3dy':
+                    this.render = this.t3dUpdate
+                    break
+                case 'scrollTop':
+                    this.render = this.scrollTopUpdate
+                    break
+                default:
+                    this.render = this.styleUpdate
+            }
+            this.isNotMultipleT = true
+            this.distance = +this.end - +this.start
+        }
+    }
+
+    multipleT (update) {
+        let t3dx = 0
+        let t3dy = 0
+        let rotate = 0
+
+        for (let i = 0; i < this.updateQty; i++) {
+            if (this.param[i] === '3dx') {
+                t3dx = S.Is.string(this.start[i]) ? update[i] + 'px' : update[i] + '%'
+            } else if (this.param[i] === '3dy') {
+                t3dy = S.Is.string(this.start[i]) ? update[i] + 'px' : update[i] + '%'
+            } else if (this.param[i] === 'rotate') {
+                rotate = 'rotate(' + update[i] + 'deg)'
+            }
+        }
+
+        const translate3d = 'translate3d(' + t3dx + ',' + t3dy + ',0)'
+        const multipleTransform = translate3d + ' ' + rotate
+
+        this.letsRock('transform', 'transform', multipleTransform)
     }
 
     attributUpdate (update) {
@@ -133,7 +183,7 @@ S.Animate = class {
             if (type === 'setAttribut') {
                 el.setAttribute(param, update)
             } else {
-                el.style[param] = update
+                el.style[param] = update + ';'
             }
         }
     }
