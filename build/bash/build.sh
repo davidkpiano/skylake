7f6b73665762327f80c037e05804ae767932ec3a
ariiiman <aristide.benoist@gmail.com>
2016-10-11 11:00:59 +0200

    1.0.47

--- build/bash/build.sh
+++ build/bash/build.sh
@@ -3,5 +3,4 @@ node build/node/concat.js
 node build/node/uglify.js
 rm -r skylake.js
 node build/node/version.js
-cp index.js ../penryn-starter/src/static/js/lib/skylake/skylake-test.js
-cp index.js ../../aristidebenoist/src/static/js/lib/skylake/skylake-test.js
+sh build/bash/copy.sh
